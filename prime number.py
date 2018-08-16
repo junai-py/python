65515e23aaad8f4957fd1252f4ccd56d8061b5fe
Arunjunai Rajan <41290080+junai96@users.noreply.github.com>
2018-08-16 22:41:27 +0530

    Update prime number.py
    To check a number is prime or not.

--- prime number.py
+++ prime number.py
@@ -1,22 +1,10 @@
-# Python program to check if the input number is prime or not
-
-num = 407
-
-# take input from the user
-# num = int(input("Enter a number: "))
-
-# prime numbers are greater than 1
+num = int(input("Enter a number: "))
 if num > 1:
-   # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
-           break
-   else:
-       print(num,"is a prime number")
-       
-# if input number is less than
-# or equal to 1, it is not prime
+    for i in range(2,num):
+        if (num%i)==0:
+            print(f'{num} is not a prime number')
+            break
+    else:
+            print(f'{num} is a prime number')
 else:
-   print(num,"is not a prime number")
+    print('Enter a number greater than 1')
